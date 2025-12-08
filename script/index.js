07e53b7a517f05add2cd2a89bb7d5f23defc69dc
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-12-08 10:12:30 +0100

    add english version of CV

--- script/index.js
+++ script/index.js
@@ -4,3 +4,15 @@ document.addEventListener('mousemove', (e) => {
     shadow.style.left = e.clientX + 'px';
     shadow.style.top = e.clientY + 'px';
 });
+
+const langToggle = document.getElementById('language-toggle');
+if (langToggle) {
+    langToggle.addEventListener('click', () => {
+        const currentPath = window.location.pathname;
+        if (currentPath.endsWith('index_eng.html')) {
+            window.location.href = 'index.html';
+        } else {
+            window.location.href = 'index_eng.html';
+        }
+    });
+}
