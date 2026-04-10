6c15993cfc8d342837f70c973e6124b361a05e41
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-04-10 15:19:25 +0200

    add palceholder text, add state and if statements to page

--- portfolio/src/routes/+page.svelte
+++ portfolio/src/routes/+page.svelte
@@ -1,9 +1,43 @@
 <script lang="ts">
+    import About from "../components/sections/about.svelte";
+    import Experience from "../components/sections/experience.svelte";
+    import Projects from "../components/sections/projects.svelte";
+    import { activeSection } from "$lib/sectionState.svelte";
 </script>
 
 <main>
     <div class="fixed-panel"></div>
-    <div class="section-panel"></div>
+    <div class="section-panel">
+        {#if activeSection.current === "about"}
+            <About />
+        {:else if activeSection.current === "experience"}
+            <Experience />
+        {:else if activeSection.current === "projects"}
+            <Projects />
+        {/if}
+    </div>
 </main>
 
-<style></style>
+<style>
+    main {
+        margin-top: 2rem;
+        margin-bottom: 2rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+        color: var(--primary-text-color);
+        overflow: hidden;
+    }
+
+    .fixed-panel {
+        width: 50%;
+        height: 100%;
+    }
+
+    .section-panel {
+        width: 50%;
+        height: 100%;
+    }
+</style>
