5ae313f4b7311666a6c16a3a823408a19f571414
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-04-10 12:39:48 +0200

    add use of state

--- portfolio/src/components/navPill.svelte
+++ portfolio/src/components/navPill.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
+    import { activeSection } from "../lib/sectionState.svelte";
+
+    function setActiveSection(section: string) {
+        activeSection.current = section;
+    }
 </script>
 
-<div class="nav-pill"></div>
+<div class="nav-pill">
+    <button onclick={() => setActiveSection("about")}>About Me</button>
+    <button onclick={() => setActiveSection("experience")}>Experience</button>
+    <button onclick={() => setActiveSection("projects")}>Projects</button>
+    <button onclick={() => setActiveSection("education")}>Education</button>
+</div>
 
 <style>
 </style>
