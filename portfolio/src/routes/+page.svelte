ebfa306c93b63325a76be709debf038d542ad804
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-04-11 09:55:55 +0200

    about on start looked good, make use of the component

--- portfolio/src/routes/+page.svelte
+++ portfolio/src/routes/+page.svelte
@@ -41,37 +41,7 @@
                 />
             </div>
 
-            <div class="about">
-                <h2 class="lighter-text">About me</h2>
-                <p>
-                    I am a curious <span class="lighter-text">.NET student</span
-                    >
-                    who finds it easy to acquire new knowledge and who loves to
-                    <span class="lighter-text">grow and improve.</span> The feeling
-                    of solving problems is unbeatable. Previously I studied chemical
-                    engineering at Chalmers, but it is programming that I am passionate
-                    about and want to devote myself to in the future.
-                </p>
-                <p>
-                    From my time in <span class="lighter-text"
-                        >customer service</span
-                    >
-                    and
-                    <span class="lighter-text">sales</span>
-                    I have learned to
-                    <span class="lighter-text">work independently</span>
-                    with great responsibility,
-                    <span class="lighter-text">think creatively</span> and
-                    <span class="lighter-text">solve problems.</span> Most of the
-                    time, it is simply a matter of asking the right questions!
-                </p>
-                <p>
-                    In my spare time I practice <span class="lighter-text"
-                        >karate</span
-                    > and wouldn't say no to a board game evening with friends. Or
-                    a beer down at the local.
-                </p>
-            </div>
+            <About />
 
             <div class="links">
                 <div class="github">
@@ -174,18 +144,6 @@
 
     .title {
         margin: 0.1rem;
-    }
-
-    .about {
-        margin: 2rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: flex-start;
-        align-items: center;
-    }
-
-    .lighter-text {
-        color: var(--secondary-text-color);
     }
 
     .profile-pic {
