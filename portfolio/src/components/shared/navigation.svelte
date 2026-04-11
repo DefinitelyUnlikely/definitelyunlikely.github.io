24265223a8a431a4df60508b2625eb56a47f79a6
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-04-11 14:26:32 +0200

    add some palceholder css variables

---
+++ portfolio/src/components/shared/navigation.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { activeSection } from "$lib/sectionStore.svelte";
+</script>
+
+<nav>
+    <div class="nav-pill">
+        <button class:active={activeSection.current === ""}>Home</button>
+        <button class:active={activeSection.current === "about"}>About</button>
+        <button class:active={activeSection.current === "experience"}
+            >Experience</button
+        >
+        <button class:active={activeSection.current === "projects"}
+            >Projects</button
+        >
+        <button class:active={activeSection.current === "skills"}>Skills</button
+        >
+        <button class:active={activeSection.current === "contact"}
+            >Contact</button
+        >
+    </div>
+</nav>
+
+<style>
+</style>
--- portfolio/src/routes/+layout.svelte
+++ portfolio/src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from "$lib/assets/favicon.svg";
+	import Navigation from "../components/shared/navigation.svelte";
 
 	let { children } = $props();
 </script>
@@ -8,4 +9,20 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<Navigation />
 {@render children()}
+
+<style>
+	:global(:root) {
+		--background-color: #000000;
+		--text-color: #ffffff;
+		--accent-color: #2f00ff;
+		--font-family: "Inter", sans-serif;
+	}
+
+	:global(body) {
+		background-color: var(--background-color);
+		color: var(--text-color);
+		font-family: var(--font-family);
+	}
+</style>
