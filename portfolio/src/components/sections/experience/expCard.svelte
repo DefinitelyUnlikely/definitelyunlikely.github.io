f0fad1ee3ab0d489d04b6047d4054c25e6daa199
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-04-11 10:17:26 +0200

    make experiences a exp card instead of html

---
+++ portfolio/src/components/sections/experience/expCard.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    let { title, time, description } = $props<{
+        title: string;
+        time: string;
+        description: string;
+    }>();
+</script>
+
+<div class="card">
+    <h3>{title}</h3>
+    <p>{time}</p>
+    <p>{description}</p>
+</div>
+
+<style>
+    .card {
+        background-color: var(--secondary-bg-color);
+        border-radius: 10px;
+        padding: 1rem;
+        margin-bottom: 1rem;
+    }
+
+    h3 {
+        color: var(--secondary-text-color);
+    }
+
+    p {
+        font-size: 1.2rem;
+        line-height: 1.6;
+        margin-bottom: 1rem;
+    }
+</style>
