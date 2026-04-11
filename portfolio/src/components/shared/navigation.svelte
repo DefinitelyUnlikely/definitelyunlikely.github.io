49af86db9b9c11b8ee0b70f41468c62374bd13a9
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-04-11 14:27:38 +0200

    add onclick functions

--- portfolio/src/components/shared/navigation.svelte
+++ portfolio/src/components/shared/navigation.svelte
@@ -4,18 +4,31 @@
 
 <nav>
     <div class="nav-pill">
-        <button class:active={activeSection.current === ""}>Home</button>
-        <button class:active={activeSection.current === "about"}>About</button>
-        <button class:active={activeSection.current === "experience"}
+        <button
+            class:active={activeSection.current === ""}
+            onclick={() => (activeSection.current = "")}>Home</button
+        >
+        <button
+            class:active={activeSection.current === "about"}
+            onclick={() => (activeSection.current = "about")}>About</button
+        >
+        <button
+            class:active={activeSection.current === "experience"}
+            onclick={() => (activeSection.current = "experience")}
             >Experience</button
         >
-        <button class:active={activeSection.current === "projects"}
+        <button
+            class:active={activeSection.current === "projects"}
+            onclick={() => (activeSection.current = "projects")}
             >Projects</button
         >
-        <button class:active={activeSection.current === "skills"}>Skills</button
+        <button
+            class:active={activeSection.current === "skills"}
+            onclick={() => (activeSection.current = "skills")}>Skills</button
         >
-        <button class:active={activeSection.current === "contact"}
-            >Contact</button
+        <button
+            class:active={activeSection.current === "contact"}
+            onclick={() => (activeSection.current = "contact")}>Contact</button
         >
     </div>
 </nav>
