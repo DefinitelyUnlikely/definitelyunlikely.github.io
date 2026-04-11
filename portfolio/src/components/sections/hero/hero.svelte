1150b90ed65657be130341bb1407244d3069a371
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-04-11 15:22:02 +0200

    profile pic

--- portfolio/src/components/sections/hero/hero.svelte
+++ portfolio/src/components/sections/hero/hero.svelte
@@ -1,1 +1,39 @@
-Hero placeholder
+<script lang="ts">
+    import { activeSection } from "$lib/sectionStore.svelte";
+    import ProfilePicture from "../../shared/images/profilePicture.svelte";
+</script>
+
+<div class="hero">
+    <div class="hero-text">
+        <h1>Martin Kåår Johansson</h1>
+        <ProfilePicture />
+        <p>Software Developer</p>
+    </div>
+</div>
+
+<style>
+    .hero {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+        width: 100%;
+    }
+
+    .hero-text {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .hero-text h1 {
+        font-size: 2rem;
+        font-weight: bold;
+    }
+
+    .hero-text p {
+        font-size: 1.5rem;
+    }
+</style>
---
+++ portfolio/src/components/shared/images/profilePicture.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import profilePicture from "$lib/assets/img/linkedin-profile-pic.jpg";
+</script>
+
+<div class="profile-pic">
+    <img id="prof-pic" src={profilePicture} alt="Martin Kåår Johansson" />
+</div>
+
+<style>
+    .profile-pic {
+        margin: 1rem 0 1rem 0;
+    }
+
+    #prof-pic {
+        width: 200px;
+        border-radius: 50%;
+        box-shadow: 0 0 3rem rgba(255, 255, 255, 0.473);
+    }
+</style>
