6ed2f0f7800f35ea9f5df12ca15e798ade713bf1
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-04-11 18:06:23 +0200

    add some styling to badges

--- portfolio/src/components/sections/skills/skills.svelte
+++ portfolio/src/components/sections/skills/skills.svelte
@@ -1,19 +1,58 @@
 <script lang="ts">
     import SkillBadge from "../../shared/skillBadge.svelte";
 
+    import azureDevopsIcon from "$lib/assets/icons/Azure Devops.svg";
+    import azureSqlIcon from "$lib/assets/icons/Azure SQL Database.svg";
+    import azureIcon from "$lib/assets/icons/Azure.svg";
+    import csharpIcon from "$lib/assets/icons/CSharp.svg";
+    import css3Icon from "$lib/assets/icons/CSS3.svg";
+    import djangoIcon from "$lib/assets/icons/Django.svg";
+    import dockerIcon from "$lib/assets/icons/Docker.svg";
+    import gitIcon from "$lib/assets/icons/Git.svg";
+    import githubActionsIcon from "$lib/assets/icons/GitHub Actions.svg";
+    import githubIcon from "$lib/assets/icons/GitHub.svg";
+    import javascriptIcon from "$lib/assets/icons/JavaScript.svg";
+    import kotlinIcon from "$lib/assets/icons/Kotlin.svg";
+    import linuxIcon from "$lib/assets/icons/Linux.svg";
+    import matlabIcon from "$lib/assets/icons/MATLAB.svg";
+    import mongodbIcon from "$lib/assets/icons/MongoDB.svg";
+    import postgresIcon from "$lib/assets/icons/PostgresSQL.svg";
+    import pythonIcon from "$lib/assets/icons/Python.svg";
+    import reactIcon from "$lib/assets/icons/React.svg";
+    import svelteIcon from "$lib/assets/icons/Svelte.svg";
+    import typescriptIcon from "$lib/assets/icons/TypeScript.svg";
+    import vscodeIcon from "$lib/assets/icons/Visual Studio Code (VS Code).svg";
+    import windows11Icon from "$lib/assets/icons/Windows 11.svg";
+    import xmlIcon from "$lib/assets/icons/XML.svg";
+    import dotnetIcon from "$lib/assets/icons/dotnet-svgrepo-com.svg";
+    import xunitIcon from "$lib/assets/icons/xunit.svg";
+
     const skills = [
-        { name: "C#", icon: "/icons/csharp.svg" },
-        { name: ".NET", icon: "/icons/dotnet.svg" },
-        { name: "ASP.NET Core", icon: "/icons/aspnetcore.svg" },
-        {
-            name: "Entity Framework Core",
-            icon: "/icons/entityframeworkcore.svg",
-        },
-        { name: "SQL Server", icon: "/icons/sqlserver.svg" },
-        { name: "Git", icon: "/icons/git.svg" },
-        { name: "GitHub", icon: "/icons/github.svg" },
-        { name: "Visual Studio", icon: "/icons/visualstudio.svg" },
-        { name: "Visual Studio Code", icon: "/icons/visualstudiocode.svg" },
+        { name: "C#", icon: csharpIcon },
+        { name: ".NET", icon: dotnetIcon },
+        { name: "xUnit", icon: xunitIcon },
+        { name: "Python", icon: pythonIcon },
+        { name: "Django", icon: djangoIcon },
+        { name: "JavaScript", icon: javascriptIcon },
+        { name: "TypeScript", icon: typescriptIcon },
+        { name: "React", icon: reactIcon },
+        { name: "Svelte", icon: svelteIcon },
+        { name: "CSS3", icon: css3Icon },
+        { name: "Kotlin", icon: kotlinIcon },
+        { name: "MATLAB", icon: matlabIcon },
+        { name: "Git", icon: gitIcon },
+        { name: "GitHub", icon: githubIcon },
+        { name: "GitHub Actions", icon: githubActionsIcon },
+        { name: "Azure DevOps", icon: azureDevopsIcon },
+        { name: "Azure", icon: azureIcon },
+        { name: "Azure SQL", icon: azureSqlIcon },
+        { name: "PostgreSQL", icon: postgresIcon },
+        { name: "MongoDB", icon: mongodbIcon },
+        { name: "Docker", icon: dockerIcon },
+        { name: "Linux", icon: linuxIcon },
+        { name: "VS Code", icon: vscodeIcon },
+        { name: "Windows 11", icon: windows11Icon },
+        { name: "XML", icon: xmlIcon },
     ];
 </script>
 
--- portfolio/src/components/shared/skillBadge.svelte
+++ portfolio/src/components/shared/skillBadge.svelte
@@ -3,9 +3,29 @@
 </script>
 
 <div class="skill-badge">
-    <img src={skill.icon} alt={skill.name} />
-    <p>{skill.name}</p>
+    <img class="skill-icon" src={skill.icon} alt={skill.name} />
+    <p class="skill-name">{skill.name}</p>
 </div>
 
 <style>
+    .skill-badge {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 2rem;
+        border-radius: 0.5rem;
+        background-color: var(--background-color);
+        border: 1px solid var(--background-color-secondary);
+        box-shadow: 0 0 0 1px #00000067;
+    }
+
+    .skill-icon {
+        height: 1.5rem;
+        width: 1.5rem;
+        margin-right: 0.5rem;
+    }
+
+    .skill-name {
+        font-size: 0.8rem;
+    }
 </style>
